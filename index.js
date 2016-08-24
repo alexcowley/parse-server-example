f29272bf0367716d4d2d5a3dee85dd9f1f406031
alexcowley <alexscowley88@gmail.com>
2016-08-24 13:41:12 -0700

    Update index.js

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ var databaseUri = 'mongodb://heroku_9b0mkch5:vu8fvo6p33nb8av0nnke8ef9ql@ds015720
 var api = new ParseServer({
     databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'myAppId',
+  appId: process.env.APP_ID || 'myAppIdskool',
   masterKey: process.env.MASTER_KEY || 'myMasterKeyskool', //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
+  serverURL: process.env.SERVER_URL || 'http://skooldatabase1.herokuapp.com/parse',  // Don't forget to change to https if needed
   
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
